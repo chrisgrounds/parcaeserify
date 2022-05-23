4d63a59279dc1ab8c3f8b5b2a2c15140d4e4bf91
chrisgrounds <iamchrisgrounds@gmail.com>
2022-05-23 11:52:44 +0100

    Use a hashmap to pre-compute alphabet conversion

--- app/Main.hs
+++ app/Main.hs
@@ -4,24 +4,36 @@ import Data.Char ( ord, chr, isUpper )
 import Control.Parallel ()
 import Control.Parallel.Strategies ( parList, rdeepseq, using )
 import Data.Time.Clock.POSIX
+import Data.Maybe
+import qualified Data.HashMap.Strict as HM
+
+createLookupTable :: Int -> HM.HashMap Char Char
+createLookupTable n =
+  let lowercase = foldr (\next acc -> acc ++ [(next, shiftedChar next n)]) [] ['a'..'z']
+      uppercase = foldr (\next acc -> acc ++ [(next, shiftedChar next n)]) [] ['A'..'Z']
+   in HM.fromList $ lowercase ++ uppercase
 
 shiftedChar :: Char -> Int -> Char
 shiftedChar c n =
   let base = if isUpper c then 'A' else 'a'
   in chr(mod ((ord c - ord base) + n) 26 + ord base)
 
-shiftedWord :: [Char] -> Int -> [Char]
-shiftedWord w n = fmap (`shiftedChar` n) w `using` parList rdeepseq
+shiftedWord :: [Char] -> HM.HashMap Char Char -> [Char]
+shiftedWord w lookupTable = fmap (\x -> fromMaybe '?' $ HM.lookup x lookupTable) w `using` parList rdeepseq
 
 caeserify :: [Char] -> Int -> [Char]
-caeserify m n = unwords (fmap (`shiftedWord` n) (words m))
+caeserify m n = 
+  let lookupTable = createLookupTable n
+  in unwords (fmap (`shiftedWord` lookupTable) (words m))
 
 main :: IO ()
 main = do
   contents <- readFile "./prideAndPrejudice.txt"
   r1 <- (getPOSIXTime :: IO POSIXTime)
   print $ "start: " ++ (show r1)
+
   let result = caeserify contents 3
+  
   r2 <- (getPOSIXTime :: IO POSIXTime)
   print $ "end: " ++ (show r2)
   print $ "time taken: " ++ (show $ r2 - r1)
